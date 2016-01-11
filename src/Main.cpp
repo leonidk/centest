04ad2de6d59bb877eb5e1281b3bb354bbe42f4a7
leonidk <lkeselman@gmail.com>
2016-01-11 15:13:56 -0800

    assume cones from middlebury exists

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,17 @@
 #include "imio.h"
 #include "cMatch.h"
-
+#include "image_filter.h"
 int main(int argc, char* argv[])
 {
-	img::Img<uint8_t> left, right;
-	stereo::CensusMatch cm(0, 0, 0);
-	auto disp = cm.match(left, right);
+    auto left = img::imread<uint8_t,3>("cones/im2.png");
+    auto right = img::imread<uint8_t,3>("cones/im6.png");
+	auto gt = img::imread<uint8_t,1>("cones/disp2.png");
+   
+    auto left_g = img::Rgb2grey(left);
+    auto right_g = img::Rgb2grey(right);
+    stereo::CensusMatch cm(left.width, left.height, 64);
+	
+    auto disp = cm.match(left_g, right_g);
+    
 	return 0;
 }
