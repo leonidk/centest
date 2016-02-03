313e12c7b1d92ee1225fc7f2c1792e7a338ca5e5
leonidk <lkeselman@gmail.com>
2016-02-03 14:42:52 -0800

    yey

--- src/cMatch.cpp
+++ src/cMatch.cpp
@@ -3,6 +3,19 @@
 
 using namespace stereo;
 
+// Census Radius and Width
+#define		C_R		(3)
+#define		C_W		(2*C_R+1)
+
+// Box Filter Radius and Width
+#define		B_R		(3)
+#define		B_W		(2*B_R+1)
+
+const int samples[] =
+{ 
+	-2,1
+};
+
 
 CensusMatch::CensusMatch(int w, int h, int d, int m)
 	: StereoMatch(w, h, d, m), costs(w*d)
