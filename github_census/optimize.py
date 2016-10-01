c0c22eaadcabd91a04ca6dae8d06921ece7c58eb
leonidk <lkeselman@gmail.com>
2016-10-01 13:03:35 -0700

    an optimization demo too

---
+++ github_census/optimize.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+#
+# Optimize blocksize of apps/mmm_block.cpp
+#
+# This is an extremely simplified version meant only for tutorials
+#
+
+import opentuner
+from opentuner import ConfigurationManipulator
+from opentuner import IntegerParameter
+from opentuner import FloatParameter
+from opentuner import MeasurementInterface
+from opentuner import Result
+import run_all
+import json
+import numpy as np
+class DTFlagsTuner(MeasurementInterface):
+
+  def manipulator(self):
+    manipulator = ConfigurationManipulator()
+    manipulator.add_parameter(
+      FloatParameter('dt_space', 1, 50))
+    manipulator.add_parameter(
+      FloatParameter('dt_range', 1, 200))
+    return manipulator
+
+  def run(self, desired_result, input, limit):
+    cfg = desired_result.configuration.data
+
+    with open('github_dt.json') as fp:
+        alg_cfg = json.load(fp)
+    alg_cfg['config'].update(cfg)
+    with open('opt.json','w') as fp:
+        alg_cfg = json.dump(alg_cfg,fp)
+    results = run_all.run_all_algs()
+    short_results = {}
+    for alg,ds in results.iteritems():
+        rz = []
+        for dsn,de in ds.iteritems():
+            for den,res in de.iteritems():
+                nd = {k:v for k,v in res.iteritems()}
+                rz.append(nd)
+        metric_names = rz[0].keys()
+        rzz = {m: np.mean([y[m]['result'] for y in rz]) for m in metric_names}
+        short_results[alg] = rzz
+    return Result(time=short_results['opt']['err_3'])
+
+  def save_final_config(self, configuration):
+    print "Optimal block size written to mmm_final_config.json:", configuration.data
+    self.manipulator().save_to_file(configuration.data,
+                                    'mmm_final_config.json')
+
+if __name__ == '__main__':
+  argparser = opentuner.default_argparser()
+  DTFlagsTuner.main(argparser.parse_args())
--- github_census/run_all.py
+++ github_census/run_all.py
@@ -33,82 +33,82 @@ def run_alg(dataset,algorithm,out_dir):
         with cd(algorithm['dir']):
             call([algorithm['command'],cfg_path])
 
-saved_file = 'finished_results.json'
-metric_results = 'metric_results.json'
-dataset_dir = '.'
-metrics_dir = '.'
-algs_dir = '.'
-out_dir = 'out'
+def run_all_algs():
+    saved_file = 'finished_results.json'
+    metric_results = 'metric_results.json'
+    dataset_dir = '.'
+    metrics_dir = '.'
+    algs_dir = '.'
+    out_dir = 'out'
 
-try:
-    with open(saved_file) as fp:
-        saved = json.load(fp)
-except:
-    saved = defaultdict(str)
+    try:
+        with open(saved_file) as fp:
+            saved = json.load(fp)
+    except:
+        saved = defaultdict(str)
 
-datasets = loadFiles(dataset_dir,'dataset')
-algorithms = loadFiles(algs_dir,'algorithm')
-metrics = loadFiles(metrics_dir,'metric')
+    datasets = loadFiles(dataset_dir,'dataset')
+    algorithms = loadFiles(algs_dir,'algorithm')
+    metrics = loadFiles(metrics_dir,'metric')
 
-check_and_make_dir(out_dir)
+    check_and_make_dir(out_dir)
 
-results = {}
-for algorithm in algorithms:
-    results[algorithm['name']] = {}
-    for dataset in datasets:
-        unique_out = os.path.join(out_dir,algorithm['name'],dataset['name'])
-        unique_exists = os.path.exists(unique_out)
-        check_and_make_dir(unique_out)
+    results = {}
+    for algorithm in algorithms:
+        results[algorithm['name']] = {}
+        for dataset in datasets:
+            unique_out = os.path.join(out_dir,algorithm['name'],dataset['name'])
+            unique_exists = os.path.exists(unique_out)
+            check_and_make_dir(unique_out)
 
-        hashkey = '_'.join([ hashlib.sha1(json.dumps(dataset)).hexdigest(), \
-                            hashlib.sha1(json.dumps(algorithm)).hexdigest(), \
-                            path_checksum([algorithm['command']] + algorithm['dependent_files']) ])
-        if (not unique_exists) or (saved[unique_out] != hashkey):
-            run_alg(dataset,algorithm,unique_out)
-            saved[unique_out] = hashkey
-            print unique_out,hashkey
-        result = {}
-        for name in dataset['names']:
-            data = extract_fields(dataset,name,unique_out)
-            result[name] = {}
-            for metric in metrics:
-                metric.update(data)
-                metric['output'] = ''
+            hashkey = '_'.join([ hashlib.sha1(json.dumps(dataset)).hexdigest(), \
+                                hashlib.sha1(json.dumps(algorithm)).hexdigest(), \
+                                path_checksum([algorithm['command']] + algorithm['dependent_files']) ])
+            if (not unique_exists) or (saved[unique_out] != hashkey):
+                run_alg(dataset,algorithm,unique_out)
+                saved[unique_out] = hashkey
+                print unique_out,hashkey
+            result = {}
+            for name in dataset['names']:
+                data = extract_fields(dataset,name,unique_out)
+                result[name] = {}
+                for metric in metrics:
+                    metric.update(data)
+                    metric['output'] = ''
 
-                #with open(metric_cfg_path,'w') as fp:
-                #    json.dump(metric,fp)
-                #print metric_cfg_path, name, unique_out
-                res = check_output([metric['command'],json.dumps(metric)])
-                resn = json.loads(res)
-                result[name].update(json.loads(res))
-                #print res#json.dumps(metric, sort_keys=True,indent=4, separators=(',', ': '))
-                #with open(metric['output'],'r') as fp:
-                #    res = json.load(fp)
-                #if name in results[unique_name]:
-                #    results[unique_name][name].update(res)
-                #else:
-                #    results[unique_name][name] = res
-        unique_name = '_'.join([dataset['name'],algorithm['name']])
-        results[algorithm['name']][dataset['name']] = result
-short_results = {}
-for alg,ds in results.iteritems():
-    rz = []
-    for dsn,de in ds.iteritems():
-        for den,res in de.iteritems():
-            nd = {k:v for k,v in res.iteritems()}
-            rz.append(nd)
-            #metric_names = res.keys()
-            #metric_values = [res[x]['result'] for x in metric_names]
-            #for mn, mr in res.iteritems():
-            #    print mr['result']
+                    #with open(metric_cfg_path,'w') as fp:
+                    #    json.dump(metric,fp)
+                    #print metric_cfg_path, name, unique_out
+                    res = check_output([metric['command'],json.dumps(metric)])
+                    resn = json.loads(res)
+                    result[name].update(json.loads(res))
+                    #print res#json.dumps(metric, sort_keys=True,indent=4, separators=(',', ': '))
+                    #with open(metric['output'],'r') as fp:
+                    #    res = json.load(fp)
+                    #if name in results[unique_name]:
+                    #    results[unique_name][name].update(res)
+                    #else:
+                    #    results[unique_name][name] = res
+            unique_name = '_'.join([dataset['name'],algorithm['name']])
+            results[algorithm['name']][dataset['name']] = result
+    with open(saved_file,'w') as fp:
+        json.dump(saved,fp)
+    with open(metric_results,'w') as fp:
+        json.dump(results,fp)
+    return results
 
-    metric_names = rz[0].keys()
-    rzz = {m: np.mean([y[m]['result'] for y in rz]) for m in metric_names}
-    short_results[alg] = rzz
-with open(saved_file,'w') as fp:
-    json.dump(saved,fp)
-with open(metric_results,'w') as fp:
-    json.dump(results,fp)
-
-print json.dumps(short_results, sort_keys=True,indent=4, separators=(',', ': '))
+if __name__ == '__main__':
+    results = run_all_algs()
+    short_results = {}
+    geomean = lambda n: reduce(lambda x,y: x*y, n) ** (1.0 / len(n))
+    for alg,ds in results.iteritems():
+        rz = []
+        for dsn,de in ds.iteritems():
+            for den,res in de.iteritems():
+                nd = {k:v for k,v in res.iteritems()}
+                rz.append(nd)
+        metric_names = rz[0].keys()
+        rzz = {m: np.mean([y[m]['result'] for y in rz]) for m in metric_names}
+        short_results[alg] = rzz
+    print json.dumps(short_results, sort_keys=True,indent=4, separators=(',', ': '))
 
